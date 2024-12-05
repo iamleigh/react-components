fbd00c8051311931819a2b57daa0e0ddf34a0a8a
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-04 23:09:44 -0500

    Downgrade Storybook
    Storybook 8 has conflicts with React stories. While resolving this issue is better downgrading to a version that works more stable with React stories, like Storybook 6.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,32 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-	"../packages/**/stories/*.mdx",
-    "../packages/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-};
-export default config;
+const path = require('path');
+
+module.exports = {
+	stories: [
+		"../packages/**/stories/*.mdx",
+    	"../packages/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)"
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-postcss",
+		"@storybook/addon-a11y"
+	],
+	webpackFinal: async (config, { configType }) => {
+		// Add SASS support.
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				'sass-loader'
+			],
+			include: path.resolve( __dirname, '../' ),
+		});
+
+		return config;
+	},
+	framework: "@storybook/react",
+	core: {
+		"builder": "@storybook/builder-webpack5"
+	}
+}
